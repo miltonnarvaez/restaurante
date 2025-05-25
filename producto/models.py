d97767662cd5271796c12d9289626db7752b3087
Milton Narvaez <mnarvaezflorez@gmail.com>
2025-05-24 23:12:04 -0500

    actualice un campo

--- producto/models.py
+++ producto/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     activo = models.BooleanField(default=False)
     fecha_creado = models.DateTimeField(auto_now=True)
 
