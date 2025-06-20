eecfb6da5077b0c549322e6c341c7f53cd6c6645
miltonnarvaez <mnarvaezflorez@gmail.com>
2025-06-19 22:19:34 -0500

    Update wsgi.py

--- tulpas/wsgi.py
+++ tulpas/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tulpas.settings')
 if os.environ.get('RENDER') == 'true':
-    from tulpas.create_superuser import *  # cambia esto por la ruta real
+
   
 application = get_wsgi_application()
