a5ea57100acc21e6323b4c38adaea727fd01d353
miltonnarvaez <mnarvaezflorez@gmail.com>
2025-06-19 22:27:09 -0500

    Update apps.py

--- producto/apps.py
+++ producto/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class ProductoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'producto'
+
+    def ready(self):
+        import producto.signals  # importa las señales aquí
